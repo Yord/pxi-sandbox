1b8fd7cbead7db7034a6615a53da882b6b6f8295
Philipp Wille <philipp.y.wille@gmail.com>
2019-12-29 09:13:59 +0100

    feat: add noHeader, fillHeader, and skipEmpty flags to csv parser

--- src/csv/parsers/csv.js
+++ src/csv/parsers/csv.js
@@ -1,10 +1,20 @@
 module.exports = {
   name: 'csv',
   desc: 'is a csv parser.', // does not support embedded line breaks
-  func: ({delimiter, pdelimiter, D, header, pheader, H}) => {
-    const _delimiter = pdelimiter || delimiter || D || ','
-    const _header    = pheader    || header    || H || '[]'
-    let keys         = JSON.parse(_header)
+  func: ({
+    delimiter,  pdelimiter,  D,
+    header,     pheader,     H,
+    noHeader,   pnoHeader,   N,
+    skipEmpty,  pskipEmpty,  E,
+    fillHeader, pfillHeader, F
+  }) => {
+    const _delimiter  = pdelimiter  || delimiter  || D || ','
+    const _header     = pheader     || header     || H || '[]'
+    const _noHeader   = pnoHeader   || noHeader   || N || false
+    const _skipEmpty  = pskipEmpty  || skipEmpty  || E || false
+    const _fillHeader = pfillHeader || fillHeader || F || false
+
+    let keys          = JSON.parse(_header)
 
     return (tokens, lines) => {
       const err   = [] // add error handling
@@ -45,7 +55,7 @@ module.exports = {
             valueFound  = false
             const value = token.slice(from, at)
 
-            values.push(value)
+            if (!_skipEmpty || value !== '') values.push(value)
 
             from        = at + 1
           }
@@ -53,25 +63,43 @@ module.exports = {
           if (at === token.length - 1) {
             const value = token.slice(from)
             
-            values.push(value)
+            if (!_skipEmpty || value !== '') values.push(value)
           }
         }
 
-        let json = {}
-
-        if (keys.length === 0) keys = values
-        else {
-          for (let jndex = 0; jndex < keys.length; jndex++) {
-            if (keys.length === values.length) {
-              const key   = keys[jndex]
-              const value = values[jndex]
-              json[key]   = value
-            } else {
-              // fail otherwise
+        if (_noHeader) {
+          if (_fillHeader) {
+            const json = {}
+            
+            for (let jndex = 0; jndex < values.length; jndex++) {
+              json[jndex] = values[jndex]
             }
+            
+            jsons.push(json)
+          } else {
+            jsons.push(values)
           }
+        } else {
+          const json = {}
+
+          if (keys.length === 0) keys = values
+          else {
+            if (_fillHeader) {
+              for (let jndex = 0; jndex < Math.max(keys.length, values.length); jndex++) {
+                const key   = jndex < keys.length   ? keys[jndex]   : jndex
+                const value = jndex < values.length ? values[jndex] : null
+                json[key]   = value
+              }
+            } else {
+              for (let jndex = 0; jndex < keys.length; jndex++) {
+                const key   = keys[jndex]
+                const value = jndex < values.length ? values[jndex] : null
+                json[key]   = value
+              }
+            }
   
-          jsons.push(json)
+            jsons.push(json)
+          }
         }
       }
 
