d053a31e38ba6c5e991ed79e5780430f90e1e224
Philipp Wille <philipp.y.wille@gmail.com>
2020-01-12 13:00:22 +0100

    feat: add coerce function, lodash, and ramda

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const xml     = require('./src/xml')
 const geojson = require('./src/geojson')
 const cata    = require('./src/cata')
 
+const coerce = str => {
+  const num = +str
+  return num === num ? num : str
+}
+
 module.exports = {
   plugins:  [
     xml,
     geojson,
     cata
   ],
-  context:  {},
+  context:  Object.assign({}, require('ramda'), {_: require('lodash'), coerce}),
   defaults: {}
 }
